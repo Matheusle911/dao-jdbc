dfa51a3fda88952d57514836fdc2369243de0ece
Matheus Oliveira <matheush3006@gmail.com>
2026-02-25 02:02:41 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,5 +10,8 @@ public class Main {
 
         Department obj = new Department(1,"books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21,"bob","Bob@gmail.com",new Date(),3000.0,obj);
+        System.out.println(seller);
     }
 }
