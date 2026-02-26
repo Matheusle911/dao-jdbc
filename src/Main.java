92a6ad10a00541adf2d2423ee40c71b5e2d9affe
Matheus Oliveira <matheush3006@gmail.com>
2026-02-25 23:27:11 -0300

    findById implementation

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
 import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Connection conn = DB.getConnection();
 
-        Department obj = new Department(1,"books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21,"bob","Bob@gmail.com",new Date(),3000.0,obj);
-
-        System.out.println(seller);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println(sellerDao.findById(3));
     }
 }
