756277aeedb8177aecbe72076a99c35c5e946381
Matheus Oliveira <matheush3006@gmail.com>
2026-02-25 22:43:43 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class Main {
         System.out.println(obj);
 
         Seller seller = new Seller(21,"bob","Bob@gmail.com",new Date(),3000.0,obj);
+
         System.out.println(seller);
     }
 }
